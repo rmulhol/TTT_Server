feb4aba423ca202d499ed973daaa6efe9bed9455
Robert Mulholand <robmulholand@gmail.com>
2015-08-03 12:51:23 -0500

    add http server functionality via jar

--- src/com/TTTServer/Main.java
+++ src/com/TTTServer/Main.java
@@ -1,8 +1,20 @@
 package com.TTTServer;
 
+import com.httpServer.Config.LoggerConfig;
+import com.httpServer.Config.RouteConfig;
+import com.httpServer.Config.ServerConfig;
+import com.httpServer.Runner.ServerRunner;
+
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+        LoggerConfig.configureLogs();
+        ServerConfig.configureServer(args);
+
+        RouteConfig.setupRoutes();
+
+        new ServerRunner().run();
     }
 }
